9e3bc0a3aee2b6e9b0f507e6d85721abc7b9c690
PatchWorkCoding <85509178+PatchWorkCoding@users.noreply.github.com>
2022-07-17 10:11:20 -0500

    Game is fully functional

--- GMTK Game Jam 2022/Assets/RangerBehavior.cs
+++ GMTK Game Jam 2022/Assets/RangerBehavior.cs
@@ -86,51 +86,6 @@ public class RangerBehavior :EnemyBehavior
 
     protected override Vector2Int[] GeneratePossibleDirections()
     {
-        List<Vector2Int> _dirs = new List<Vector2Int>();
-        if (target.x != index.x)
-        {
-            if (target.x > index.x)
-            {
-                _dirs.Add(new Vector2Int(1, 0));
-                spriteRenderer.transform.localScale = new Vector3(1, 1, 1);
-            }
-            else
-            {
-                _dirs.Add(new Vector2Int(-1, 0));
-                spriteRenderer.transform.localScale = new Vector3(-1, 1, 1);
-            }
-        }
-        else if (target.y != index.y)
-        {
-            if (target.y > index.y)
-            {
-                _dirs.Add(new Vector2Int(0, 1));
-                spriteRenderer.transform.localScale = new Vector3(-1, 1, 1);
-            }
-            else
-            {
-                _dirs.Add(new Vector2Int(0, -1));
-                spriteRenderer.transform.localScale = new Vector3(1, 1, 1);
-            }
-        }
-
-        if (_dirs[0].y != 0)
-        {
-            _dirs.Add(new Vector2Int(1, 0));
-            _dirs.Add(new Vector2Int(-1, 0));
-            _dirs.Add(new Vector2Int(0, -_dirs[0].y));
-        }
-
-        else if (_dirs[0].x != 0)
-        {
-            _dirs.Add(new Vector2Int(0, 1));
-            _dirs.Add(new Vector2Int(0, -1));
-            _dirs.Add(new Vector2Int(-_dirs[0].x, 0));
-        }
-
-        return _dirs.ToArray();
-
-        /*
         List<Vector2Int> _returnDirs = new List<Vector2Int>();
 
         Vector2Int _prefferedDir = Vector2Int.zero;
@@ -195,7 +150,6 @@ public class RangerBehavior :EnemyBehavior
         }
 
         return _returnDirs.ToArray();
-        */
     }
 
     int CollapseToOne(int _a, int _b)
